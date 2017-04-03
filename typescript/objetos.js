64df95fe616d21334a32c5f76d50b74fec558780
yojotaman <yojotaman@gmail.com>
2017-04-03 08:48:00 -0500

    interpolación, mezcla de arrays con strings

--- typescript/objetos.js
+++ typescript/objetos.js
@@ -1,13 +1,22 @@
 // indica que vamos a empezar una clase(lista de variables y funciones con las que podemos trabajar)
 var MyPerson = (function () {
     //constructor de la clase
-    function MyPerson(_first_name, _last_name) {
-        console.log(_first_name, _last_name);
+    function MyPerson(first_name, last_name) {
+        console.log("first_name", first_name);
+        console.log("last_name", last_name);
+        this.first_name = first_name;
+        this.last_name = last_name;
     }
+    MyPerson.prototype.getSaludo = function () {
+        var emojis = ':p';
+        return "Saludos:\n        " + this.first_name + " " + this.last_name + ",\n        te enviamos un emoji de la consola" + emojis;
+    };
     return MyPerson;
 }());
-//instancia de unaa clase (objeto). Cuando no se especifican los parámetros, estos aparecen como undefined
-
-var personaUno = new MyPerson();
-var personaDos = new MyPerson('Jonathan');
+//instancia de unaa clase (objeto)
+//let personaUno = new MyPerson();
+//let personaDos = new MyPerson('Jonathan'); 
+//let personaTres = new MyPerson('Jonathan', 'Salazar');  
+// interpolación: la forma de mezclar variables con strings
 var personaTres = new MyPerson('Jonathan', 'Salazar');
+console.log(personaTres.getSaludo());
--- typescript/objetos.ts
+++ typescript/objetos.ts
@@ -4,12 +4,26 @@ class MyPerson {
     last_name:string;
 
     //constructor de la clase
-    constructor(_first_name?:string, _last_name?:string){// el interogante indica que no es un parámetro obligatorio
-        console.log(_first_name, _last_name)
+    constructor(first_name?:string, last_name?:string){// el interogante indica que no es un parámetro obligatorio
+        console.log("first_name", first_name);
+        console.log("last_name", last_name);
+        this.first_name = first_name;
+        this.last_name = last_name;        
+    }
+    getSaludo():string{
+        let emojis=':p';
+        return `Saludos:
+        ${this.first_name} ${this.last_name},
+        te enviamos un emoji de la consola` + emojis
     }
 }
 
 //instancia de unaa clase (objeto)
-let personaUno = new MyPerson();
-let personaDos = new MyPerson('Jonathan'); 
-let personaTres = new MyPerson('Jonathan', 'Salazar');  
+//let personaUno = new MyPerson();
+//let personaDos = new MyPerson('Jonathan'); 
+//let personaTres = new MyPerson('Jonathan', 'Salazar');  
+
+// interpolación: la forma de mezclar variables con strings
+var personaTres = new MyPerson('Jonathan', 'Salazar');
+
+console.log(personaTres.getSaludo());
