f6f0dce55dfa836372cf23604bb983d58880dbef
yojotaman <yojotaman@gmail.com>
2017-04-04 16:13:22 -0500

    Routing entre estados

--- angular/proyectOne/src/app/services/ticket.service.ts
+++ angular/proyectOne/src/app/services/ticket.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { TICKETS } from './mocks/tickets.mock';
+
+import {Observable} from 'rxjs/Observable';
+
 @Injectable()
 export class TicketService{
 
@@ -16,6 +19,14 @@ export class TicketService{
     getTicket(id:number){
         let ticket = TICKETS.find(x => x.id === id);
         return ticket;
+    }
+
+     getTicketObserver(id) {
+        return Observable.create(observer => {
+            setTimeout(() => {
+            observer.next(TICKETS.find((ticket) => ticket.id == id))
+            }, 3000);
+        });
     }
 
 }
