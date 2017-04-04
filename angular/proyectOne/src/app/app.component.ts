45bfc78c1bfe8d7256f32a12746bea43e9750f04
yojotaman <yojotaman@gmail.com>
2017-04-04 14:14:07 -0500

    ejemplo básico de programacion reactiva usando redux y uso de RxJS con NGRx con Angular

--- angular/proyectOne/src/app/app.component.ts
+++ angular/proyectOne/src/app/app.component.ts
@@ -6,6 +6,14 @@ import {TicketService} from './services/ticket.service';
 
 import { FormBuilder, FormGroup } from '@angular/forms';
 
+import { Store } from '@ngrx/store';
+import { INCREMENT, DECREMENT, RESET } from './services/counter';
+import {Observable} from 'rxjs/Observable';
+
+interface AppState {
+  counter: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -24,10 +32,14 @@ export class AppComponent {
 
   myForm: FormGroup;
 
+  counter: Observable<number>;
+
   constructor (
     private ticketService: TicketService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private store: Store<AppState>
     ){
+    this.counter = store.select('counter');
     this.tickets = ticketService.getTickets(); // Aquí puedo hacer el llamado del metodo para //obtener todos los ticktets  
     // valor por defecto
     this.myForm = fb.group({
@@ -36,6 +48,17 @@ export class AppComponent {
     
   }
   
+   increment(){
+        this.store.dispatch({ type: INCREMENT });
+    }
+
+    decrement(){
+        this.store.dispatch({ type: DECREMENT });
+    }
+
+    reset(){
+        this.store.dispatch({ type: RESET });
+    }
 
   addVoto(response:string){
     this.votacion = "Usted eligió: " + response;
